ec4c3a1fdd3d4d5dae5bf45b7971ba0034b03879
Umut Gultekin <umut.gultekn@hotmail.com>
2022-01-05 21:27:03 +0300

    Initial Commit

---
+++ CoreDataExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataExample
+//
+//  Created by Umut Gultekin on 5.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
